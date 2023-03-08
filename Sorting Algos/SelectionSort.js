8e6d5d44d8bdbf5784e613a19c740a371512b696
Anahis Valenzuela <Anahisvq7@gmail.com>
2023-03-07 18:44:31 -0800

    selection sort algorithm

--- Sorting Algos/SelectionSort.js
+++ Sorting Algos/SelectionSort.js
@@ -1,16 +1,17 @@
 const selectionSort = array => {
-    for (let i = 0; i < array.length; i++){
-        let smallestNum = array[i]
-        let indexOfSmallestNum
-      for (let j = i + 1; j < array.length; j++){
-          if (smallestNum > array[j]){
-            smallestNum = array[j]
-            indexOfSmallestNum = j
-          }
+  for (let i = 0; i < array.length; i++) {
+    let smallestNum = array[i]
+    let idxOfSmall = i
+    for (let j = i + 1; j < array.length; j++) {
+      if (smallestNum > array[j]) {
+        smallestNum = array[j]
+        idxOfSmall = j
       }
-      const bigger = array[i]
-      array[i] = smallestNum
-      array[indexOfSmallestNum] = bigger
     }
-    return array
+    [array[i], array[idxOfSmall]] = [array[idxOfSmall], array[i]]
   }
+  return array
+}
+
+// time complexity: o(n^2)
+// space complexity: o(1)
