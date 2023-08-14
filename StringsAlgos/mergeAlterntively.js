42ac27a5b69f0ad0e9c5494c90119ded89f236ed
Anahis Valenzuela <Anahisvq7@gmail.com>
2023-08-14 14:46:24 -0700

    merge strings algo

---
+++ StringsAlgos/mergeAlterntively.js
@@ -0,0 +1,20 @@
+const mergeAlternately = (word1, word2) => {
+    const merged = [] 
+  
+    let longest = word1.length > word2.length ? word1 : word2
+    let shortest = word1.length < word2.length ? word1 : word2
+
+    for (let i = 0; i < shortest.length; i++){
+      merged.push(word1[i])
+      merged.push(word2[i])
+    } 
+
+    if(longest){
+     merged.push(longest.slice(shortest.length))
+    }
+
+    return merged.join("")
+}
+
+// time: o(n + m)
+// space: o(n + m)
