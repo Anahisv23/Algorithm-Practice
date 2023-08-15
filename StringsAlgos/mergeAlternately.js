32784af86fa3a2903c7cda063ebb0cbe3bdbf7c3
Anahis Valenzuela <Anahisvq7@gmail.com>
2023-08-15 15:44:13 -0700

    updated approach for merge alternately

--- StringsAlgos/mergeAlternately.js
+++ StringsAlgos/mergeAlternately.js
@@ -1,20 +1,18 @@
-const mergeAlternately = (word1, word2) => {
-    const merged = [] 
-  
-    let longest = word1.length > word2.length ? word1 : word2
-    let shortest = word1.length < word2.length ? word1 : word2
+const mergeAlternatively = (word1, word2) => {
+  const merged = []  
 
-    for (let i = 0; i < shortest.length; i++){
-      merged.push(word1[i])
-      merged.push(word2[i])
-    } 
+  let longest = word1.length > word2.length ? word1 : word2 
+  let shortest = word1.length < word2.length ? word1 : word2 
 
-    if(longest){
-     merged.push(longest.slice(shortest.length))
-    }
+  for (let i = 0; i < shortest.length; i++){
+    merged.push(word1[i], word2[i])
+  } 
 
-    return merged.join("")
+  if (longest !== shortest){
+   merged.push(longest.slice(shortest.length))
+  }
+
+  return merged.join("")
 }
-
 // time: o(n + m)
 // space: o(n + m)
