7c74ba9821122daf11cba3e9b28e087344acf37e
Anahis Valenzuela <Anahisvq7@gmail.com>
2023-10-11 14:02:40 -0700

    added notes to best time to sell and buy stock algo for better understanding

--- ArrayAlgos/BestTimetoSell&BuyStock.js
+++ ArrayAlgos/BestTimetoSell&BuyStock.js
@@ -7,9 +7,12 @@ const maxProfit = (prices) => {
     let maxProfit = 0; 
 
     for (let i = 1; i < prices.length; i++) {
-        if (prices[i] < minPrice) {
+        // if a smaller minimum is found update min price so we buy stock for cheaper
+        if (prices[i] < minPrice) { 
             minPrice = prices[i]; 
-        } else {
+        } else { 
+        // if currNum is greater than minimum then come here and caluculate curr profit 
+        // if our currentProfit is greater than our maxProfit update it since we want to get as much profit as we can 
             let currentProfit = prices[i] - minPrice;
             if (currentProfit > maxProfit) {
                 maxProfit = currentProfit; 
