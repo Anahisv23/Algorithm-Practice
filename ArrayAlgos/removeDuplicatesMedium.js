396ed59baee80cbf7a9349304062a8fb4d14981c
Anahis Valenzuela <Anahisvq7@gmail.com>
2023-11-14 16:52:38 -0800

    new approach to solve remove duplicates

--- ArrayAlgos/removeDuplicatesMedium.js
+++ ArrayAlgos/removeDuplicatesMedium.js
@@ -1,16 +1,16 @@
 const removeDuplicates = (nums) => {
     let k = 0
-    let dupeCounter = 0 
+    let dupeCounter = 0
 
-    for(let i = 0; i < nums.length; i++){
-        for(let j = i + 1; j < nums.length; j++){
-            if(nums[i] === nums[j] && dupeCounter < 1){
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j] && dupeCounter < 1) {
                 dupeCounter += 1
-            } else if (nums[i] === nums[j] && dupeCounter === 1){
+            } else if (nums[i] === nums[j] && dupeCounter === 1) {
                 nums[j] = "_"
-                for(let a = j; a < nums.length - 1; a++){
+                for (let a = j; a < nums.length - 1; a++) {
                     //swap 
-                    let temp = nums[a]; 
+                    let temp = nums[a];
                     nums[a] = nums[a + 1];
                     nums[a + 1] = temp;
                 }
@@ -22,28 +22,36 @@ const removeDuplicates = (nums) => {
 
     const idx = nums.indexOf("_")
     k = idx !== -1 ? idx : nums.length
-  
-    return k  
+
+    return k
 };
 
 // time: o(n^2)
 // space: o(1)
 
 // optimized 
+
 const removeDuplicates2 = (nums) => {
     if (nums.length <= 2) {
         return nums.length;
     }
+    
+    let k = 2;
+    // Traverse elements through loop...
+    // we swap to move non unique elem to the end and increment k number of unique chars at the same time
 
-    let slow = 2;
-    for (let fast = 2; fast < nums.length; fast++) {
-        if (nums[fast] !== nums[slow - 2]) {
-            nums[slow] = nums[fast];
-            slow++;
+    // the conditional on line 48 will not increment k counter if
+    // curr elem equals the number before or the equal the number before before
+
+    for (let i = 2; i < nums.length; i++) {
+        // If the index does not match the (k-1)th and (k-2)th elements, count that element...
+        if (nums[i] != nums[k - 2] || nums[i] != nums[k - 1]) {
+            nums[k] = nums[i];
+            k++;
+            // If the index matches the (k-1)th and (k-2)th elements, we skip it...
         }
     }
-
-    return slow;
+    return k;       //Return k after placing the final result in the first k slots of nums...
 };
 
 // time: o(n)
