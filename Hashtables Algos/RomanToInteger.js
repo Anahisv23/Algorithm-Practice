299ccf3fa1c4765da5e72aab89e7f725a2db78ba
Anahis Valenzuela <Anahisvq7@gmail.com>
2024-01-22 15:01:08 -0800

    roman numeral integer algo

--- Hashtables Algos/RomanToInteger.js
+++ Hashtables Algos/RomanToInteger.js
@@ -1,25 +1,49 @@
 const romanToInteger = string => {
-    let result = 0
-    const hashmap = {
-      I: 1, 
-      V: 5,
-      X: 10, 
-      L: 50, 
-      C: 100, 
-      D: 500,
-      M: 1000
-    }
-  
-    for(let i = 0; i < string.length; i++){
-      if(hashmap[string[i]] < hashmap[string[i + 1]]){
-        result += hashmap[string[i + 1]] - hashmap[string[i]] 
-        i = i + 1 
-      } else {
-        result += hashmap[string[i]]
-      }
-    }
-    return result 
+  let result = 0
+  const hashmap = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
   }
+
+  for (let i = 0; i < string.length; i++) {
+    if (hashmap[string[i]] < hashmap[string[i + 1]]) {
+      result += hashmap[string[i + 1]] - hashmap[string[i]]
+      i = i + 1
+    } else {
+      result += hashmap[string[i]]
+    }
+  }
+  return result
+}
 
 // time complexity: o(n) linear runtime 
 // space complexity: o(1) constant space 
+
+const romanToInt = s => {
+  let integerTotal = 0
+  const hashmap = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    let currRomanInteger = s[i]
+    if (hashmap[currRomanInteger] < hashmap[s[i + 1]]) {
+      integerTotal += hashmap[s[i + 1]] - hashmap[currRomanInteger]
+      i = i + 1
+    } else {
+      integerTotal += hashmap[currRomanInteger]
+    }
+  }
+  return integerTotal
+};
