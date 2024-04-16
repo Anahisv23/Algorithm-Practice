6163dceff6cec3f930525f6e0e89cc6e1a4fcfdf
Anahis Valenzuela <anahisvq7@gmail.com>
2024-04-15 18:04:14 -0700

    mergeSortedArray2 algo

---
+++ ArrayAlgos/mergeSortedArray2.js
@@ -0,0 +1,15 @@
+const merge = (nums1, m, nums2, n) => {
+    let p2 = 0
+
+    for (let i = m; i < nums1.length; i++){
+        nums1[i] = nums2[p2]
+        p2 += 1 
+    }
+
+    nums1.sort((a, b) => a - b)
+
+    return nums1
+};
+
+// time: o(m log m)
+// space: o(1)
