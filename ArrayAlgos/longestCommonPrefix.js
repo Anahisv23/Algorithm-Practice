a8febf4d4271922114f8ce19ee2aca6df22f77ac
Anahis Valenzuela <anahisvq7@gmail.com>
2024-04-23 14:24:44 -0700

    changed prefix var to str

--- ArrayAlgos/longestCommonPrefix.js
+++ ArrayAlgos/longestCommonPrefix.js
@@ -1,24 +1,27 @@
 const longestCommonPrefix = strs => {
-    const prefix = [strs[0]] 
+    let prefix = strs[0]
 
     for (let i = 1; i < strs.length; i++){
-        const shortestLen = strs[i].length < prefix[0].length ? strs[i].length : prefix[0].length
+        const shortestLen = strs[i].length < prefix.length ? strs[i].length : prefix.length
 
             let newPrefix = "" 
             
             for (let j = 0; j < shortestLen; j++){
-                if(strs[i][j] === prefix[0][j]){
+                if(strs[i][j] === prefix[j]){
                     newPrefix += strs[i][j]
-                } else if(strs[i][j] !== prefix[0][j]){
-                    prefix[0] = newPrefix
+                } else if(strs[i][j] !== prefix[j]){
+                    prefix = newPrefix
                 }
        }
-         prefix[0] = newPrefix
+         prefix = newPrefix
          newPrefix = ""
     }
 
-    return prefix[0]
+    return prefix
 };
+
+// time: o(n * m)
+// space: o(1)
 
 // time: o(n * m)
 // space: o(1)
